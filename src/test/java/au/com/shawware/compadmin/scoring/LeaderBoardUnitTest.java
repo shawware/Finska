5e6b138558f1464076bb5ceb7688e1e9b5c729cd
David Shaw <david.shaw@shawware.com.au>
2018-04-25 10:15:42 +1000

    Remove old, redundant tests.

--- src/test/java/au/com/shawware/compadmin/scoring/LeaderBoardUnitTest.java
+++ src/test/java/au/com/shawware/compadmin/scoring/LeaderBoardUnitTest.java
@@ -27,66 +27,6 @@ import au.com.shawware.compadmin.entity.TestEntrant;
 public class LeaderBoardUnitTest extends AbstractScoringUnitTest
 {
     /**
-     * Test the basic leader board generation algorithm for a simple case.
-     */
-    @Test
-    public void testSimpleCase()
-    {
-        final Number[][] results = new Number[][]
-        {
-            { 1, 3, 0, 1, -2.0, 1 },
-            { 2, 3, 3, 3,  3.0, 9 },
-            { 3, 3, 1, 2,  0.0, 4 },
-            { 4, 3, 1, 1, -1.0, 3 },
-        };
-        final int[][] ranks = new int[][]
-        {
-            { 2, 1 },
-            { 3, 2 },
-            { 4, 3 },
-            { 1, 4 },
-        };
-
-        verifyCase(results, ranks);
-    }
-
-    /**
-     * Test the leader board generation algorithm for a complex case.
-     * That is, one where sorting and ranking involves more than just
-     * the points and we have equal ranks.
-     */
-    @Test
-    public void testComplexCase()
-    {
-        final Number[][] results = new Number[][]
-        {
-            { 1, 9, 5, 20,  5.2, 16 },
-            { 2, 9, 1,  3, -2.0, 10 },
-            { 3, 9, 8, 30, 20.0, 24 },
-            { 4, 9, 2,  3, -1.0, 10 },
-            { 5, 9, 4, 19,  5.2, 16 },
-            { 6, 9, 3, 18,  5.2, 16 },
-            { 7, 9, 7, 25, 10.5, 21 },
-            { 8, 9, 6, 25, 10.5, 21 },
-            { 9, 9, 0,  5, -5.0,  0 },
-        };
-        final int[][] ranks = new int[][]
-        {
-            { 3, 1 },
-            { 7, 2 },
-            { 8, 2 },
-            { 1, 4 },
-            { 5, 5 },
-            { 6, 6 },
-            { 4, 7 },
-            { 2, 8 },
-            { 9, 9 },
-        };
-
-        verifyCase(results, ranks);
-    }
-
-    /**
      * Test the algorithms in {@link AbstractLeaderBoardAssistant}.
      */
     @Test
@@ -179,48 +119,6 @@ public class LeaderBoardUnitTest extends AbstractScoringUnitTest
             Assert.assertEquals(id, expectedResults[i][ 8].intValue(),    actualResult.getResultItemValueAsInt(TestResultItems.GOAL_DIFF));
             Assert.assertEquals(id, expectedResults[i][ 9].doubleValue(), actualResult.getResultItemValueAsDouble(TestResultItems.GOAL_PERC), 0.0001);
             Assert.assertEquals(id, expectedResults[i][10].intValue(),    actualResult.getResultItemValueAsInt(TestResultItems.POINTS));
-        }
-    }
-
-    /**
-     * Verify a single case, ie. generate a leader board from the given
-     * inputs and verify that they match the expected outputs.
-     * 
-     * @param input the input results
-     * @param output the expected ranks
-     */
-    private void verifyCase(Number[][] input, int[][] output)
-    {
-        Assert.assertNotNull(input);
-        Assert.assertNotNull(output);
-        Assert.assertEquals(input.length, output.length);
-
-        ILeaderBoardAssistant assistant = new TestAssistant(sCompetition);
-        List<EntrantResult> results = assistant.compileOverallResults();
-        Assert.assertNotNull(results);
-        Assert.assertEquals(input.length, results.size());
-
-        List<EntrantResult> leaderBoard = LeaderBoardGenerator.generateLeaderBoard(assistant);
-
-        verifyRanking(leaderBoard, output);
-    }
-
-    /**
-     * Verify the given ranked results are as expected.
-     * 
-     * @param rankedResults the ranked results
-     * @param expectedRanks the expected ranks
-     */
-    private void verifyRanking(List<EntrantResult> rankedResults, int[][] expectedRanks)
-    {
-        Assert.assertNotNull(rankedResults);
-        Assert.assertEquals(expectedRanks.length, rankedResults.size());
-        for (int i = 0; i < expectedRanks.length; i++)
-        {
-            EntrantResult result = rankedResults.get(i);
-            Assert.assertNotNull(result);
-            Assert.assertEquals(expectedRanks[i][0], result.getEntrantID());
-            Assert.assertEquals(expectedRanks[i][1], result.getRank());
         }
     }
 
